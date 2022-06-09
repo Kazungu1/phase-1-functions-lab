261d90bc8f65964853685df5b0edb441e9cabe41
Kazungu1 <kazunguderriq@gmail.com>
2022-06-09 17:25:30 +0300

    commit tree 28758418d0857b0046d4063ee4a46f1b0d93dc7a

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ function distanceFromHqInBlocks(distance) {
 function distanceFromHqInFeet(distance){
     return distanceFromHqInBlocks(distance)*264;
 }
+function distanceTravelledInFeet(start,destination){
+    if (start < destination){
+        return (destination-start)*264;
+    }
+    else{
+        return (start-destination)*264;
+    }
+}
+function calculatesFarePrice(start, destination){
+    let charges;
+    if (distanceTravelledInFeet(start, destination)<=400){
+        return 0;
+    }else if(distanceTravelledInFeet(start, destination)>400 && distanceTravelledInFeet(start, destination)<=2000){
+        charges= (distanceTravelledInFeet(start, destination)-400)*0.02;
+    }else if(distanceTravelledInFeet(start, destination)>2000 && distanceTravelledInFeet(start, destination)<=2500){
+        charges= 25;
+    }else{
+        return "cannot travel that far";
+    }
+    return charges;
+}
